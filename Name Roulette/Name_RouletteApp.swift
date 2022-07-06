24ec2db4d5daa3363a43f08025c424b6c0b564c5
Usman Nazir <usmannzr@live.com>
2022-07-06 12:17:34 +0100

    Initial Commit

---
+++ Name Roulette/Name_RouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Name_RouletteApp.swift
+//  Name Roulette
+//
+//  Created by Usman on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Name_RouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
